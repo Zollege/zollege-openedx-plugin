00c72c855b3af58e74bc3bd405f135393da657a7
Luis Moreno <luis.moreno@edunext.co>
2020-12-01 16:16:03 -0400

    🌅

---
+++ zollege_openedx_plugin/apps.py
@@ -0,0 +1,44 @@
+"""
+App configuration for zollege_openedx_plugin.
+"""
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ZollegePluginConfig(AppConfig):
+    """
+    Zollege Open edX plugin configuration.
+    """
+    name = 'zollege_openedx_plugin'
+    verbose_name = 'Zollege Open edX plugin'
+
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': 'zollege_openedx_plugin',
+                'regex': r'^zollege_openedx_plugin/',
+                'relative_path': 'urls',
+            },
+            'cms.djangoapp': {
+                'namespace': 'zollege_openedx_plugin',
+                'regex': r'^zollege_openedx_plugin/',
+                'relative_path': 'urls',
+            }
+        },
+        'settings_config': {
+            'lms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'test': {'relative_path': 'settings.test'},
+                'aws': {'relative_path': 'settings.aws'},
+                'production': {'relative_path': 'settings.production'},
+            },
+            'cms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'test': {'relative_path': 'settings.test'},
+                'aws': {'relative_path': 'settings.aws'},
+                'production': {'relative_path': 'settings.production'},
+            },
+        }
+    }
---
+++ zollege_openedx_plugin/settings/test.py
@@ -0,0 +1,7 @@
+"""
+Test Django settings for zollege_openedx_plugin project.
+"""
+
+from __future__ import unicode_literals
+
+from .common import *  # pylint: disable=wildcard-import
